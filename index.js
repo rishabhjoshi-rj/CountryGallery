06f4af5256548a88b22c2b278838ad2ad26e1417
rishabhjoshi-rj <72876100+rishabhjoshi-rj@users.noreply.github.com>
2020-10-28 23:48:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ request.onload = function () {
     data.forEach((country) => {
       const card = document.createElement("div");
       card.setAttribute("class", "card col-sm-12 col-md-6 col-lg-3");
-      //   card.setAttribute('style', 'padding : 2ren');
 
-      //   card.classList.add('thumbnail');
-      //   card.classList.add('cl-sm-3');
+      const butrj = document.createElement("button");
+      butrj.textContent = "Know More";
+      butrj.setAttribute("class", "rjbt btn btn-info");
 
       const img = document.createElement("IMG");
       img.setAttribute("id", "imgs");
@@ -41,11 +41,13 @@ request.onload = function () {
       h4.textContent = `Language: ${lang}`;
 
       container.appendChild(card);
+
       card.appendChild(img);
       card.appendChild(h1);
       card.appendChild(h2);
       card.appendChild(h3);
       card.appendChild(h4);
+      card.appendChild(butrj);
     });
   } else {
     const errorMessage = document.createElement("marquee");
@@ -53,121 +55,106 @@ request.onload = function () {
     app.appendChild(errorMessage);
   }
 
-  // const html  = `
-  // Name: ${.name}
+  var top = document.querySelector("#top");
 
-  // `;
-  // document.getElementById('top').insertAdjacentElement('afterend', html);
+  var bottom = document.querySelector("#bottom");
 
-  //****************************************
+  top.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "Know More") {
+      // e2= Required Event, when clicken on button, it will take that event but we need data from
+      //its parent e.target, that is card, so we use below e.target.parentElement
+      e2 = e.target.parentElement;
+      console.log(e.target.parentElement);
 
-  x = document.querySelector("#top");
+      top.style.display = "none";
+      bottom.style.display = "block";
 
-  y = document.querySelector("#bottom");
+      b = document.createElement("button");
+      b.setAttribute("id", "btn");
+      b.setAttribute("class", "btn btn-success");
+      b.textContent = "GO BACK";
 
-  x.addEventListener("click", (e) => {
-    x.style.display = "none";
-    y.style.display = "block";
+      // Country Name of particular click
+      const img = e2.querySelector("img").src;
+      console.log(img);
 
-    b = document.createElement("button");
-    b.setAttribute("id", "btn");
-    b.setAttribute("class", "btn btn-success");
-    b.textContent = "GO BACK";
+      var country = e2.querySelector("h1").innerHTML;
+      countryName = country.substring(6);
+      console.log(countryName);
 
-    // Country Name of particular click
-    const img = e.target.querySelector("img").src;
-    console.log(img);
+      console.log(e.target);
+      const c = data.filter((country) => country.name === countryName);
+      console.log(c);
+      h = document.createElement("h1");
+      h.setAttribute("class", "display-4");
+      h.setAttribute("style", "text-align:center");
+      h.innerHTML = `Name: ${countryName}`;
 
-    var country = e.target.querySelector("h1").innerHTML;
-    countryName = country.substring(6);
-    console.log(countryName);
+      h2 = document.createElement("h1");
+      h2.setAttribute("class", "display-4");
+      h2.setAttribute("style", "text-align:center");
+      h2.innerHTML = `Capital: ${c[0].capital}`;
 
-    console.log(e.target);
-    const c = data.filter((country) => country.name === countryName);
-    console.log(c);
-    h = document.createElement("h1");
-    h.setAttribute("class", "display-4");
-    h.setAttribute("style", "text-align:center");
-    h.innerHTML = `Name: ${countryName}`;
+      h3 = document.createElement("h1");
+      h3.setAttribute("class", "display-4");
+      h3.setAttribute("style", "text-align:center");
+      h3.innerHTML = `Currency: ${c[0].currencies[0].name}`;
 
-    h2 = document.createElement("h1");
-    h2.setAttribute("class", "display-4");
-    h2.setAttribute("style", "text-align:center");
-    h2.innerHTML = `Capital: ${c[0].capital}`;
+      h4 = document.createElement("h1");
+      h4.setAttribute("class", "display-4");
+      h4.setAttribute("style", "text-align:center");
+      h4.innerHTML = `Calling Code: ${c[0].callingCodes}`;
 
-    h3 = document.createElement("h1");
-    h3.setAttribute("class", "display-4");
-    h3.setAttribute("style", "text-align:center");
-    h3.innerHTML = `Currency: ${c[0].currencies[0].name}`;
+      h5 = document.createElement("h1");
+      h5.setAttribute("class", "display-4");
+      h5.setAttribute("style", "text-align:center");
+      h5.innerHTML = `Native Name: ${c[0].nativeName}`;
 
-    h4 = document.createElement("h1");
-    h4.setAttribute("class", "display-4");
-    h4.setAttribute("style", "text-align:center");
-    h4.innerHTML = `Calling Code: ${c[0].callingCodes}`;
+      h6 = document.createElement("h1");
+      h6.setAttribute("class", "display-4");
+      h6.setAttribute("style", "text-align:center");
+      h6.innerHTML = `Population: ${c[0].population}`;
 
-    h5 = document.createElement("h1");
-    h5.setAttribute("class", "display-4");
-    h5.setAttribute("style", "text-align:center");
-    h5.innerHTML = `Native Name: ${c[0].nativeName}`;
+      h7 = document.createElement("h1");
+      h7.setAttribute("class", "display-4");
+      h7.setAttribute("style", "text-align:center");
+      h7.innerHTML = `Region: ${c[0].region}`;
 
-    h6 = document.createElement("h1");
-    h6.setAttribute("class", "display-4");
-    h6.setAttribute("style", "text-align:center");
-    h6.innerHTML = `Population: ${c[0].population}`;
+      h8 = document.createElement("h1");
+      h8.setAttribute("class", "display-4");
+      h8.setAttribute("style", "text-align:center");
+      h8.innerHTML = `SubRegion: ${c[0].subregion}`;
 
-    h7 = document.createElement("h1");
-    h7.setAttribute("class", "display-4");
-    h7.setAttribute("style", "text-align:center");
-    h7.innerHTML = `Region: ${c[0].region}`;
+      h9 = document.createElement("h1");
+      h9.setAttribute("class", "display-4");
+      h9.setAttribute("style", "text-align:center");
+      h9.innerHTML = `Area: ${c[0].area}`;
 
-    h8 = document.createElement("h1");
-    h8.setAttribute("class", "display-4");
-    h8.setAttribute("style", "text-align:center");
-    h8.innerHTML = `SubRegion: ${c[0].subregion}`;
+      const image = document.createElement("IMG");
+      image.setAttribute("id", "img2");
+      image.src = img;
 
-    h9 = document.createElement("h1");
-    h9.setAttribute("class", "display-4");
-    h9.setAttribute("style", "text-align:center");
-    h9.innerHTML = `Area: ${c[0].area}`;
+      bottom.appendChild(b);
+      bottom.appendChild(image);
+      bottom.appendChild(h);
+      bottom.appendChild(h2);
+      bottom.appendChild(h3);
+      bottom.appendChild(h4);
+      bottom.appendChild(h5);
+      bottom.appendChild(h6);
+      bottom.appendChild(h7);
+      bottom.appendChild(h8);
+      bottom.appendChild(h9);
+      //   }
+      // });
 
-    const image = document.createElement("IMG");
-    image.setAttribute("id", "img2");
-    image.src = img;
-
-    y.appendChild(b);
-    y.appendChild(image);
-    y.appendChild(h);
-    y.appendChild(h2);
-    y.appendChild(h3);
-    y.appendChild(h4);
-    y.appendChild(h5);
-    y.appendChild(h6);
-    y.appendChild(h7);
-    y.appendChild(h8);
-    y.appendChild(h9);
-  });
-
-  // function myFunction() {
-  //     x.style.display = "none";
-  //     y.style.display="block"
-
-  //     b=document.createElement('button');
-  //     b.setAttribute('id','btn');
-  //     b.setAttribute('class','btn btn-success');
-  //     b.textContent="GO BACK";
-
-  //     const p =document.createElement('h1');
-  //     p.innerHTML="Hello";
-  //     y.appendChild(b);
-  //     y.appendChild(p);
-
-  // }
-
-  document.querySelector("#bottom").addEventListener("click", (e) => {
-    if (x.style.display === "none") {
-      x.style.display = "block";
+      document.querySelector("#btn").addEventListener("click", (e) => {
+        if (top.style.display === "none") {
+          top.style.display = "block";
+        }
+        bottom.innerHTML = "";
+      });
     }
-    y.innerHTML = "";
   });
 };
 request.send();
